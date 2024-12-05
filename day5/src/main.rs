b9c0e9381f0a323f085ccd1f96bafc7b0fbfa185
Gurglemurgle5 <95941332+gurglemurgle5@users.noreply.github.com>
2024-12-05 11:26:52 -0600

    more optimizations

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -4,17 +4,16 @@ fn main() {
     let now = Instant::now();
 
     let input = fs::read_to_string("./input.txt").unwrap();
-    let (rules, updates) = input.trim().split_once("\n\n").unwrap();
+    let (rules, updates) = input.split_once("\n\n").unwrap();
 
-    let rules: Vec<(i32, i32)> = rules
+    let rules: Vec<(u8, u8)> = rules
         .lines()
         .map(|line| {
             let rule = line.split_once('|').unwrap();
             (rule.0.parse().unwrap(), rule.1.parse().unwrap())
         })
         .collect();
-
-    let updates: Vec<Vec<i32>> = updates
+    let updates: Vec<Vec<u8>> = updates
         .lines()
         .map(|line| line.split(',').map(|num| num.parse().unwrap()).collect())
         .collect();
@@ -27,27 +26,27 @@ fn main() {
         let mut is_valid = true;
         let mut should_loop = true;
 
-        // ok dumb idea, let's try looping over all the rules and swapping the placement of invalid pages
         while should_loop {
             should_loop = false;
             for rule in rules.iter() {
-                let indices = update
-                    .iter()
-                    .position(|page| *page == rule.0)
-                    .zip(update.iter().position(|page| *page == rule.1));
-                if let Some((pos0, pos1)) = indices {
-                    if pos0 > pos1 {
-                        is_valid = false;
-                        (update[pos0], update[pos1]) = (update[pos1], update[pos0]);
-                        should_loop = true;
+                // Getting the position of values seems to be the slowest, maybe it could be optimized?
+                let pos0 = update.iter().position(|page| *page == rule.0);
+                if let Some(pos0) = pos0 {
+                    let pos1 = update.iter().position(|page| *page == rule.1);
+                    if let Some(pos1) = pos1 {
+                        if pos0 > pos1 {
+                            is_valid = false;
+                            (update[pos0], update[pos1]) = (update[pos1], update[pos0]);
+                            should_loop = true;
+                        }
                     }
                 }
             }
         }
         if is_valid {
-            sum_valid += update[update.len() / 2];
+            sum_valid += update[update.len() / 2] as u16;
         } else {
-            sum_invalid += update[update.len() / 2];
+            sum_invalid += update[update.len() / 2] as u16;
         }
     }
 
