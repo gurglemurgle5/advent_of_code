3b633dc15cb96f9b3d5fc2a5745984ce22f5d305
Gurglemurgle5 <95941332+gurglemurgle5@users.noreply.github.com>
2024-12-06 12:43:51 -0600

    cursed bit math

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -3,16 +3,17 @@ use std::{fs, time::Instant};
 fn main() {
     let now = Instant::now();
 
-    let mut bitfields = [0u128; 100];
+    let mut bitfields = [0u8; 256 * 32];
 
     let input = fs::read_to_string("./input.txt").unwrap();
     let (rules, updates) = input.split_once("\n\n").unwrap();
 
     rules.lines().for_each(|line| {
         let line = line.as_bytes();
-        let left = (line[0] & 0b1111) * 10 + (line[1] & 0b1111);
-        let right = (line[3] & 0b1111) * 10 + (line[4] & 0b1111);
-        bitfields[right as usize] |= 1 << left;
+        let left = (line[0] & 0xf) << 4 | (line[1] & 0xf);
+        let right = (line[3] & 0xf) << 4 | (line[4] & 0xf);
+        bitfields[right as usize * 32 + ((left as usize & 0b1111_1000) >> 3)] |=
+            1 << (left & 0b0111);
     });
     dbg!(now.elapsed());
 
@@ -23,7 +24,7 @@ fn main() {
             .split(',')
             .map(|num| {
                 let num = num.as_bytes();
-                (num[0] & 0b1111) * 10 + (num[1] & 0b1111)
+                (num[0] & 0xf) << 4 | (num[1] & 0xf)
             })
             .collect();
         let mut is_valid = true;
@@ -32,11 +33,17 @@ fn main() {
         // more optimization
         let mut left = 0;
         while left < (update.len() - 1) {
-            if bitfields[update[left] as usize] >> (update[left + 1]) & 1 == 1 {
+            if bitfields
+                [update[left] as usize * 32 + ((update[left + 1] as usize & 0b1111_1000) >> 3)]
+                >> (update[left + 1] & 0b0111)
+                & 1
+                == 1
+            {
                 is_valid = false;
                 (update[left], update[left + 1]) = (update[left + 1], update[left]);
                 if left != 0 {
                     // We need to go back to check if the previous order is invalid
+                    // hmm maybe this could be sped up with better backtracking logic
                     left -= 1
                 } else {
                     left += 1
@@ -47,9 +54,11 @@ fn main() {
         }
 
         if is_valid {
-            sum_valid += update[update.len() / 2] as u16;
+            let num = update[update.len() / 2];
+            sum_valid += ((num >> 4) * 10 + (num & 0xf)) as u16;
         } else {
-            sum_invalid += update[update.len() / 2] as u16;
+            let num = update[update.len() / 2];
+            sum_invalid += ((num >> 4) * 10 + (num & 0xf)) as u16;
         }
     });
 
