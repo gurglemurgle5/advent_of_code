14b93c2f655c885b8c25005e72667f9d73ab3b72
Gurglemurgle5 <95941332+gurglemurgle5@users.noreply.github.com>
2024-12-06 09:54:49 -0600

    more bit math

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -3,31 +3,35 @@ use std::{fs, time::Instant};
 fn main() {
     let now = Instant::now();
 
-    let mut bitflags = [0u128; 100];
+    let mut bitfields = [0u128; 100];
 
     let input = fs::read_to_string("./input.txt").unwrap();
     let (rules, updates) = input.split_once("\n\n").unwrap();
 
     rules.lines().for_each(|line| {
-        let rule = line.split_once('|').unwrap();
-        bitflags[rule.1.parse::<usize>().unwrap()] |= 1 << rule.0.parse::<u128>().unwrap();
+        let line = line.as_bytes();
+        let left = (line[0] as u8 & 0b1111) * 10 + (line[1] as u8 & 0b1111);
+        let right = (line[3] as u8 & 0b1111) * 10 + (line[4] as u8 & 0b1111);
+        bitfields[right as usize] |= 1 << left;
     });
-    let updates: Vec<Vec<u8>> = updates
-        .lines()
-        .map(|line| line.split(',').map(|num| num.parse().unwrap()).collect())
-        .collect();
-
     dbg!(now.elapsed());
 
     let mut sum_valid = 0;
     let mut sum_invalid = 0;
-    for mut update in updates {
+    updates.lines().for_each(|line| {
+        let mut update: Vec<u8> = line
+            .split(',')
+            .map(|num| {
+                let num = num.as_bytes();
+                (num[0] as u8 & 0b1111) * 10 + (num[1] as u8 & 0b1111)
+            })
+            .collect();
         let mut is_valid = true;
 
         for i in 0..(update.len() - 1) {
             let mut j = i + 1;
             while j < update.len() {
-                if bitflags[update[i] as usize] & (1 << update[j]) == (1 << update[j]) {
+                if bitfields[update[i] as usize] & (1 << update[j]) == (1 << update[j]) {
                     is_valid = false;
                     (update[i], update[j]) = (update[j], update[i]);
                     j = i + 1;
@@ -42,7 +46,8 @@ fn main() {
         } else {
             sum_invalid += update[update.len() / 2] as u16;
         }
-    }
+    });
+
     dbg!(now.elapsed());
 
     println!("{sum_valid}");
