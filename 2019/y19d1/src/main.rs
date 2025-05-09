c44827bec580da1c23a3a2242ad2c3a5b6e12c68
Gurglemurgle <95941332+gurglemurgle5@users.noreply.github.com>
2025-05-09 16:39:22 +0000

    2019 day 1 part 2

--- 2019/y19d1/src/main.rs
+++ 2019/y19d1/src/main.rs
@@ -5,7 +5,21 @@ fn main() {
 
     let total: i32 = input
         .lines()
-        .map(|line| line.parse::<i32>().unwrap() / 3 - 2)
+        .map(|line| {
+            let mut fuel = line.parse::<i32>().unwrap() / 3 - 2;
+
+            let mut remaining = fuel;
+
+            loop {
+                let required = remaining / 3 - 2;
+                if required <= 0 {
+                    break;
+                }
+                fuel += required;
+                remaining = required;
+            }
+            fuel
+        })
         .sum();
 
     dbg!(total);
