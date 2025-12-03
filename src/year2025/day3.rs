cde8db727877a4c0c671be9d8eb72706408e0d10
Gurglemurgle5 <95941332+gurglemurgle5@users.noreply.github.com>
2025-12-03 01:04:56 -0600

    optimizations for 2025 day 3

--- src/year2025/day3.rs
+++ src/year2025/day3.rs
@@ -15,17 +15,19 @@ impl Day for Day3 {
             .lines()
             .map(|line| {
                 let len = line.len();
-                let left = line.chars().take(len - 1).max().unwrap();
-                let left_index = line
-                    .chars()
-                    .enumerate()
-                    .find_map(|(i, char)| if char == left { Some(i) } else { None })
-                    .unwrap();
+                let (left_index, left) =
+                    line.chars()
+                        .enumerate()
+                        .take(len - 1)
+                        .fold(
+                            (0, '\x00'),
+                            |old, new| if new.1 > old.1 { new } else { old },
+                        );
 
                 let right = line.chars().skip(left_index + 1).max().unwrap();
-                format!("{left}{right}").parse::<u32>().unwrap()
+                ((left as u8 - b'0') * 10 + (right as u8 - b'0')) as u16
             })
-            .sum::<u32>()
+            .sum::<u16>()
             .to_string()
     }
 
@@ -34,24 +36,19 @@ impl Day for Day3 {
             .lines()
             .map(|line| {
                 let len = line.len();
-                let mut chars = Vec::new();
-                let mut indicies = Vec::new();
+                let mut skip = 0;
 
-                for i in 0..12 {
-                    let skip = if i == 0 { 0 } else { indicies[i - 1] + 1 };
-                    let size = len - skip - (11 - i);
-                    let left = line.chars().skip(skip).take(size).max().unwrap();
-                    let left_index = line
-                        .chars()
-                        .enumerate()
-                        .skip(skip)
-                        .take(size)
-                        .find_map(|(i, char)| if char == left { Some(i) } else { None })
-                        .unwrap();
-                    chars.push(left);
-                    indicies.push(left_index);
-                }
-                chars.iter().collect::<String>().parse::<u64>().unwrap()
+                (0..12)
+                    .map(|i| {
+                        let size = len - skip - (11 - i);
+                        let (index, max) = line.chars().enumerate().skip(skip).take(size).fold(
+                            (0, '\x00'),
+                            |old, new| if new.1 > old.1 { new } else { old },
+                        );
+                        skip = index + 1;
+                        max
+                    })
+                    .fold(0, |num, char| num * 10 + (char as u8 - b'0') as u64)
             })
             .sum::<u64>()
             .to_string()
@@ -65,15 +62,8 @@ mod tests {
     #[test]
     fn test_part1() {
         assert_eq!(
-            Day3::init(
-                "987654321111111
-811111111111119
-234234234234278
-818181911112111
-"
-                .trim()
-            )
-            .part1(),
+            Day3::init("987654321111111\n811111111111119\n234234234234278\n818181911112111")
+                .part1(),
             "357"
         );
     }
@@ -81,15 +71,8 @@ mod tests {
     #[test]
     fn test_part2() {
         assert_eq!(
-            Day3::init(
-                "987654321111111
-811111111111119
-234234234234278
-818181911112111
-"
-                .trim()
-            )
-            .part2(),
+            Day3::init("987654321111111\n811111111111119\n234234234234278\n818181911112111")
+                .part2(),
             "3121910778619"
         );
     }
